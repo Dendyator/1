891d2714aa276733e96b3d0fe98c8cf5a7d3022b
Den <ravenbobby@gmail.com>
2024-02-22 17:05:12 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,44 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
+var letter []string
+var lastLetter string
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func remove(s []string) []string {
+	s = s[:len(s)-1]
+	return s
+}
+
+func Unpack(input string) (string, error) {
+	for i := 10; i <= 99; i++ {
+		re := regexp.MustCompile(strconv.Itoa(i))
+		if re.MatchString(input) == true {
+			return "", ErrInvalidString
+		}
+	}
+	part := []rune(input)
+	if len(part) == 0 {
+		return "", nil
+	}
+	if strings.IndexRune("0123456789", part[0]) != -1 {
+		return "", ErrInvalidString
+	}
+	for _, symbol := range part {
+		if strings.IndexRune("0123456789", symbol) == -1 {
+			letter = append(letter, string(symbol))
+			lastLetter = string(symbol)
+		} else {
+			letter = remove(letter)
+			num := strings.IndexRune("0123456789", symbol)
+			letter = append(letter, strings.Repeat(lastLetter, num))
+		}
+	}
+	a := strings.Join(letter, "")
+	return a, nil
 }
