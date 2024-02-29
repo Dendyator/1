b5f1f4bed5de8fe63db842eb784883ca5e705da1
Den <ravenbobby@gmail.com>
2024-02-29 16:26:21 +0300

    hw3 is complete

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,32 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func Top10(text string) []string {
+	words := strings.Fields(text)
+	scoreMap := make(map[string]int)
+	for _, word := range words {
+		scoreMap[word]++
+	}
+	if len(text) == 0 {
+		return nil
+	}
+	sortedNumbers := make([][2]string, 0, len(scoreMap))
+	for word, number := range scoreMap {
+		sortedNumbers = append(sortedNumbers, [2]string{word, strconv.Itoa(number)})
+	}
+	sort.Slice(sortedNumbers, func(i, j int) bool {
+		return sortedNumbers[i][1] > sortedNumbers[j][1] || (sortedNumbers[i][1] == sortedNumbers[j][1]) && sortedNumbers[i][0] < sortedNumbers[j][0]
+	})
+	sortedNumbers = sortedNumbers[:10]
+	result := []string{}
+	for i := 0; i < 10; i++ {
+		word := sortedNumbers[i][0]
+		result = append(result, word)
+	}
+	return result
 }
