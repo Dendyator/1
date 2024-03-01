b571b8f1afe5bbc629ae1400589192a402fc3071
Den <ravenbobby@gmail.com>
2024-03-01 14:40:57 +0300

    hw3 is complete

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -37,7 +37,7 @@ func Top10(text string) []string {
 		topWords = topWords[:10]
 	}
 
-	var result []string
+	result := []string{}
 	for i := 0; i < len(topWords); i++ {
 		result = append(result, topWords[i].word)
 	}
