4204ce018c926a09699a4a110a037986f912166f
Den <ravenbobby@gmail.com>
2024-03-03 17:27:06 +0300

    hw02 is complete

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -8,18 +8,18 @@ import (
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(input string) (string, error) { //nolint:gocognit
+func Unpack(input string) (string, error) {
 	a := strings.Builder{}
 	part := []rune(input)
 	for k, v := range part {
 		if unicode.IsDigit(v) && k == 0 || unicode.IsDigit(v) && unicode.IsDigit(part[k-1]) {
 			return "", ErrInvalidString
 		}
-		if (k < len(part)-1 && unicode.IsLetter(v) &&
-			(unicode.IsDigit(part[k+1]) == false)) || (k == len(part)-1 && unicode.IsLetter(v)) { //nolint:gosimple
+		if k < len(part)-1 && unicode.IsLetter(v) &&
+			!unicode.IsDigit(part[k+1]) || (k == len(part)-1 && unicode.IsLetter(v)) {
 			a.WriteRune(v)
 		}
-		if k < len(part)-1 && unicode.IsSpace(v) && (unicode.IsDigit(part[k+1]) == false) { //nolint:gosimple
+		if k < len(part)-1 && unicode.IsSpace(v) && !unicode.IsDigit(part[k+1]) {
 			a.WriteRune(v)
 		}
 		if (unicode.IsDigit(v) && unicode.IsLetter(part[k-1])) || (unicode.IsDigit(v) && unicode.IsSpace(part[k-1])) {
