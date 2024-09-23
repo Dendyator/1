9a0fa5162f736746b948e9214ea467f34edbcbb8
Den <ravenbobby@gmail.com>
2024-09-23 18:55:08 +0300

    check

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -3,24 +3,50 @@ package main
 import (
 	"os"
 	"os/exec"
+	"path/filepath"
 	"testing"
 )
 
+func TestRunInvalidCmd(t *testing.T) {
+	cmd := []string{"invalid_command"}
+	env := Environment{}
+
+	returnCode := RunCmd(cmd, env)
+	if returnCode == 0 {
+		t.Error("Ожидался код завершения >0 для неверной команды")
+	}
+}
+
 func TestRunCmd(t *testing.T) {
-	err := os.Setenv("TEST_VAR", "test_value")
+	tempDir, err := os.MkdirTemp("", "envdir_test_run")
 	if err != nil {
-		t.Fatalf("Переменная окружения не установлена: %v", err)
+		t.Fatal(err)
+	}
+	defer func(path string) {
+		err2 := os.RemoveAll(path)
+		if err2 != nil {
+			t.Fatal(err2)
+		}
+	}(tempDir)
+
+	if err3 := os.WriteFile(filepath.Join(tempDir, "TEST_VAR"), []byte("test_value"), 0644); err3 != nil {
+		t.Fatal(err3)
+	}
+
+	env := map[string]EnvValue{
+		"TEST_VAR": {"test_value", false},
 	}
 
 	cmd := exec.Command("cmd", "/C", "echo %TEST_VAR%")
+	cmd.Env = envToSlice(env)
 
-	output, err2 := cmd.CombinedOutput()
-	if err2 != nil {
-		t.Fatalf("Ошибка при запуске команды: %v", err2)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("failed to run command: %v", err)
 	}
 
 	expectedOutput := "test_value\r\n"
 	if string(output) != expectedOutput {
-		t.Errorf("Ожидаемый результат '%s', полученный результат '%s'", expectedOutput, output)
+		t.Errorf("Ожидаемый результат '%s', получено '%s'", expectedOutput, output)
 	}
 }
