b4672fb91869e2b04b9ec2a4c5bfddf41debc5a4
Dendyator <ravenbobby@gmail.com>
2024-10-01 20:43:24 +0300

    check executor

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -7,27 +7,29 @@ import (
 // RunCmd запускает команду + аргументы (cmd) с переменными окружения из env..
 func RunCmd(cmd []string, env Environment) (returnCode int) {
 	command := exec.Command(cmd[0], cmd[1:]...) //nolint
-
 	command.Env = envToSlice(env)
 
-	err := command.Start()
-	if err != nil {
+	if err := command.Start(); err != nil {
 		return 1
 	}
-
-	err = command.Wait()
-	if err != nil {
+	if err := command.Wait(); err != nil {
 		return 1
 	}
-
 	return 0
 }
 
 func envToSlice(env Environment) []string {
 	var result []string
+
 	for k, v := range env {
 		if !v.NeedRemove {
 			result = append(result, k+"="+v.Value)
+		}
+	}
+
+	for k, v := range env {
+		if v.NeedRemove {
+			result = append(result, k+"=")
 		}
 	}
 	return result
