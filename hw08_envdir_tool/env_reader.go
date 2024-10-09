39b704bb4232bb2d5250b99fecf97664fdb41fec
Dendyator <ravenbobby@gmail.com>
2024-10-09 14:51:21 +0300

    Update env_reader.go

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -32,12 +32,10 @@ func ReadDir(dir string) (Environment, error) {
 			return nil, err
 		}
 
-		lines := strings.SplitN(string(content), "\n", 2)
-		firstLine := lines[0]
-		cleanedValue := strings.TrimRight(firstLine, " \t")
-		cleanedValue = strings.ReplaceAll(cleanedValue, "\x00", "")
-
-		env[file.Name()] = EnvValue{Value: cleanedValue, NeedRemove: len(cleanedValue) == 0}
+		value := strings.ReplaceAll(string(content), "\x00", "")
+		value = strings.TrimRight(value, " \t")
+		
+		env[file.Name()] = EnvValue{Value: value, NeedRemove: len(value) == 0}
 	}
 
 	return env, nil
